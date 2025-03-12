31ca4eab807add3f6689f556d040d3c629c9d88d
Dilshan Herath <137473394+MadusankhaHerath@users.noreply.github.com>
2025-03-12 23:11:07 +0530

    small linkedlist

---
+++ node.java
@@ -0,0 +1,45 @@
+class Node {
+    int data;
+    Node next;
+
+    public Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+class LinkedList {
+    private Node head;
+
+    // Insert a new node at the end
+    public void insert(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node temp = head;
+            while (temp.next != null) {
+                temp = temp.next;
+            }
+            temp.next = newNode;
+        }
+    }
+
+    // Display the linked list
+    public void display() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " -> ");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+        list.insert(10);
+        list.insert(20);
+        list.insert(30);
+        list.display(); // Output: 10 -> 20 -> 30 -> null
+    }
+}
